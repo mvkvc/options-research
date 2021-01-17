9fe00946e890bc0e2617a9df959eeec09e43c1ab
Marko Vukovic <mvukovic.cloud@gmail.com>
2021-01-17 00:23:53 -0500

    Created min var hedge file for benchmark

---
+++ minvar_hedge.py
@@ -0,0 +1,41 @@
+# Benchmark from "Optimal Delta Hedging for Options" by John Hull and Alan White
+
+#
+del_f = sig_mv * del_S + eps
+
+#
+del_mv = del_bs + v_bs / S * sqrt(T) * (a + b * del_bs + c * del_bs^2)
+
+def black_scholes(self, parameter_list):
+    """
+    docstring
+    """
+    pass
+
+def calc_del_mv(self, parameter_list):
+    """
+    docstring
+    """
+    pass
+
+def delta_hedge(self, parameter_list):
+    """
+    docstring
+    """
+    pass
+
+def main(self, parameter_list):
+    """
+    docstring
+    """
+    # Set conditions for simulation
+    timestep = 0
+    #Initial conditions set
+    for item in range:
+        # Calculate hedge
+        # Rebalance
+        timestep =+ 1
+    pass
+
+if __name__ == '__main__':
+    main()
