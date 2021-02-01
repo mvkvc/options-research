4df2e0400827cc3235e3308175bdac03aafe693d
mvkvc <mvukovic.cloud@gmail.com>
2021-02-01 03:08:16 -0500

    Changed mv hedge approach

--- minvar_hedge.py
+++ minvar_hedge.py
@@ -1,10 +1,13 @@
 # %%
+from os import X_OK
 import numpy as np
+from numpy.polynomial.polynomial import Polynomial
 import pandas as pd
 from tqdm import trange
 
 # %%
 filepath = "train_data/SPX_C.csv" # "train_data/SPX_P.csv"
+est_days = 36 * 30
 
 # %%
 def est_constants():
@@ -29,105 +32,53 @@ def mv_hedging():
         num_rows = sum(1 for line in f)
 
     df = pd.read_csv(filepath) # TODO: Add terms for estimating a, b, c, in df
-    
-    row_acc   = 0
+    df["option_changed"] = (df[["exdate", "strike price"]].shift(-1) != df[["exdate", "strike price"]]).all(axis=1)
+    df["T"] = df["time to maturity"] / 365
+    df["del_f"] = df["option price"].diff()
+    df["del_S"] = df["underlying price"].diff()
+    df["regr_y"] = (df["del_f"] - df["delta"] * df["del_S"]) / ((df["vega"]/np.sqrt(df["T"])) * (df["del_S"]/df["underlying_price"]))
+    df["del_mv"] = -1
+    df["err_mv"] = -1
+
+    row_acc = 0
     month_acc = 0
+    
+    for row in trange(1, num_rows - 1):
 
-    for row in trange(1, num_rows):
-
-        compare = (
-            df[["exdate", "strike price"]].iloc[row]
-            == df[["exdate", "strike price"]].iloc[row - 1]
-        )
-        if compare.all():
-            row_acc =+ 1
+        if df["option_changed"].iloc[row]:
+            row_acc = -1
+            month_acc = -1
         else:
-            row_acc = 0
+            row_acc = row_acc + 1
+        print(row_acc)
 
-        if row_acc < 1080:
-            pass
-        elif row_acc == 1080:
+        if row_acc == est_days:
             month_acc = 0
-        elif row_acc > 1080:
-            month_acc += 1
+        elif row_acc > est_days:
+            month_acc = month_acc + 1
         else:
-            # TODO: Check how to address potential error
+            pass
 
         if month_acc == 31:
             month_acc = 0
 
-
         if month_acc == 0:
-           # TODO: Recalculate a, b, c
-           pass
+           # TODO: Calculate a, b, c with est_days
+           X = df["delta"].iloc[row - est_days - 1:row]
+           y = df["regr_y"].iloc[row - est_days - 1:row]
+           u = Polynomial(X, y, 2)
 
-
-        if row_acc >= 1080:
+        if row_acc >= est_days:
             # TODO: Apply calculated formula for error and record in row
-            pass
-
-        print(row, row_acc, month_acc)
-
-        # S = df.underlying_price[row]
-        # K = df.strike[row]
-        # r = interest_rate
-        # today = datetime.strptime(df.quote_date[row], "%Y-%m-%d").date()
-        # expiration = datetime.strptime(df.expiration[row], "%Y-%m-%d").date()
-        # T = (expiration - today).days / 365
-
-        # if row == start_row or (row - start_row) % rebalance_interval == 0:
-
-        #     price_today = prices.index[prices["quote_date"] == today.strftime("%#m/%#d/%Y")].to_list()[0] # Replace # in strftime with - in Unix
-        #     if price_today < vol_lookback:
-        #         price_lookback = 0
-        #     else:
-        #         price_lookback = price_today - vol_lookback
-        #     sigma = np.sqrt(np.var(prices.underlying_price[price_lookback:price_today+1]))
-
-        #     del_bs = df.delta[row]
-        #     # del_bs = calc_del_bs(S, K, T, r, sigma, dividend_yield)
-        #     vega_bs = calc_vega_bs(S, T, del_bs, gamma_a)
-        #     del_mv = calc_del_mv(S, T, del_bs, vega_bs)
-
-        #     port_shares = port_shares + (del_mv * contract_multiplier)
-        #     port_cash = port_cash - (del_mv * contract_multiplier) * S
-    
-        # df.port_val[row] = port_shares * S
-
-        # if start_row == row:
-        #     port_error = 0
-        # else:
-        #     chng_option = (df.option_price[row]  - df.option_price[row - 1])
-        #     chng_port = (df.port_val[row] - df.port_val[row - 1])
-        #     port_error = (chng_option - chng_port) / chng_option
-        #     print(chng_option, chng_port)
-
-        # df.port_error[row] = port_error
-        # print(port_error)
-        # print(row == start_row)
+            df["del_mv"].iloc[row] = -1
+            df["err_mv"].iloc[row] = -1
 
     return
 
-
-# %%
-mv_hedging()
 # %%
 def main():
-
-    df = sim_del_hedge(
-        filepath,
-        filepath_prices,
-        rebalance_interval,
-        starting_cash,
-        interest_rate,
-        dividend_yield,
-        contract_multiplier,
-        vol_lookback,
-    )
-    df.to_csv("minvar_results.csv")
-
+    pass
 
 # %%
 if __name__ == "__main__":
     main()
-    hello(sys.argv[1], sys.argv[2])
